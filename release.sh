3b4980937465a36bee1e5b36ff4f68b9da9c2471
David Strawn <isomarcte@gmail.com>
2021-02-17 11:53:19 -0700

    Release Candidate 1
    Add release.sh Script

---
+++ release.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+
+declare PUBLISH='NO'
+
+if git update-index --refresh && git diff-index --quiet @ --
+then
+    # Publish locally first for the scripted tests.
+    sbt '+publishLocal'
+    sbt ';clean;scalafixAll --check;scalafmtSbtCheck;scalafmtCheckAll;test;scripted;doc;test:doc;'
+    read -r -p 'Continue with publish? Type (YES):' PUBLISH
+    if [ "${PUBLISH:?}" = 'YES' ]
+    then
+        sbt '+publish'
+    else
+        echo "${PUBLISH} is not YES. Aborting." 1>&2
+    fi
+else
+    echo 'Uncommited local changes. Aborting' 1>&2
+    exit 1
+fi
