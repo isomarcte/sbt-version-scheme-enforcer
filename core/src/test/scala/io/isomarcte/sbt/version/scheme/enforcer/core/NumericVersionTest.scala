aad40e52db4810931040e06c611cc2737d703bd2
David Strawn <isomarcte@gmail.com>
2021-02-17 13:12:11 -0700

    Drop All Values After The Tag When Parsing
    Coursier Version parses "0.0.0.1-RC1" as `Vector(0, 0, 0, 1 RC, 1)`. We want to drop all the values after the tag, but also record if the version was tagged.
    
    The git automatic previous version code was also changed to attempt to find the first _non-tagged_ (in the version sense) tag (in the git sense), and then fallback to the first tagged version. This is because when working with some version "0.0.0.2-RC2" users are likely interested comparing to "0.0.0.1" _not_ "0.0.0.2-RC1".

--- core/src/test/scala/io/isomarcte/sbt/version/scheme/enforcer/core/NumericVersionTest.scala
+++ core/src/test/scala/io/isomarcte/sbt/version/scheme/enforcer/core/NumericVersionTest.scala
@@ -1,19 +1,25 @@
 package io.isomarcte.sbt.version.scheme.enforcer.core
 
 import coursier.version._
-import io.isomarcte.sbt.version.scheme.enforcer.core.SafeEquals._
 import munit._
 
 final class NumericVersionTest extends FunSuite {
 
   test("Correctly removes tags") {
-    assert(
-      NumericVersion.fromCoursierVersion(Version("1.0.0-SNAPSHOT")) ===
-        Right(NumericVersion.unsafeFromVector(Vector(BigInt(1), BigInt(0), BigInt(0))))
+    assertEquals(
+      NumericVersion.fromCoursierVersion(Version("1.0.0-SNAPSHOT")),
+      Right(NumericVersion.unsafeFromVectorIsTag(Vector(BigInt(1), BigInt(0), BigInt(0)), true))
     )
   }
 
   test("Disallows negative versions") {
     assert(NumericVersion.fromVector(Vector(BigInt(-1), BigInt(0), BigInt(0))).isLeft)
+  }
+
+  test("Drop all values after the first tag") {
+    assertEquals(
+      NumericVersion.fromString("0.0.0.1-RC2"),
+      Right(NumericVersion.unsafeFromVectorIsTag(Vector(0, 0, 0, 1).map(value => BigInt(value)), true))
+    )
   }
 }
