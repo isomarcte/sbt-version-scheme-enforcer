37076c7b107cdfe29dfd8e55ffe878c45260fa15
David Strawn <isomarcte@gmail.com>
2021-05-09 10:29:10 -0600

    Scalafmt

--- project/JREMajorVersion.scala
+++ project/JREMajorVersion.scala
@@ -16,7 +16,8 @@ object JREMajorVersion {
   val majorVersion: String =
     sys
       .props
-      .get("java.version").map(_.takeWhile(_ != '-')) // handles things like '17-ea'
+      .get("java.version")
+      .map(_.takeWhile(_ != '-')) // handles things like '17-ea'
       .fold(
         throw new RuntimeException(
           "Unable to determine JRE major version since java.version is unset, which should not be possible for standard JVMs."
