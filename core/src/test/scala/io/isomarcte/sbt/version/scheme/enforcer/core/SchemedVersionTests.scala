ae983f6657f36ce9e38fe84a64a0df5245ef21c5
Mike Miller <michael.miller@banno.com>
2025-10-22 10:41:04 -0500

    fmt test

--- core/src/test/scala/io/isomarcte/sbt/version/scheme/enforcer/core/SchemedVersionTests.scala
+++ core/src/test/scala/io/isomarcte/sbt/version/scheme/enforcer/core/SchemedVersionTests.scala
@@ -6,29 +6,17 @@ import coursier.version._
 final class SchemedVersionTests extends FunSuite {
   test("SchemedVersionTests compare") {
     val versionCompat = VersionCompatibility.apply("pvp").get
-    val oldVersion = SchemedVersion.fromVersionAndScheme(Version("1.9.0.0"), versionCompat)
-    val newVersion = SchemedVersion.fromVersionAndScheme(Version("1.10.0.0"), versionCompat)
-    assertEquals(
-      oldVersion.compare(newVersion),
-      -1
-    )
+    val oldVersion    = SchemedVersion.fromVersionAndScheme(Version("1.9.0.0"), versionCompat)
+    val newVersion    = SchemedVersion.fromVersionAndScheme(Version("1.10.0.0"), versionCompat)
+    assertEquals(oldVersion.compare(newVersion), -1)
 
-    assertEquals(
-      newVersion.compare(oldVersion),
-      1
-    )
+    assertEquals(newVersion.compare(oldVersion), 1)
 
     val three = SchemedVersion.fromVersionAndScheme(Version("1.0.0"), versionCompat)
-    val four = SchemedVersion.fromVersionAndScheme(Version("1.0.0.0"), versionCompat)
+    val four  = SchemedVersion.fromVersionAndScheme(Version("1.0.0.0"), versionCompat)
 
-    assertEquals(
-      four.compare(three),
-      2
-    )
+    assertEquals(four.compare(three), 2)
 
-    assertEquals(
-      three.compare(four),
-      -2
-    )
+    assertEquals(three.compare(four), -2)
   }
 }
